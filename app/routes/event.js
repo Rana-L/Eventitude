a33f198a16f6a71764787b249eefa0bc49b65a2d
Rehman Liaqat <rehma@MacBook-Pro.lan>
2024-10-29 18:01:54 +0000

    initial commit

---
+++ app/routes/event.js
@@ -0,0 +1,13 @@
+
+const event = require("../controllers/event.server.controllers.js")
+
+module.exports = function(app){
+    app.route("")
+        .get();
+
+    app.route("")
+        .get();
+
+    app.route("")
+        .get();
+}
---
+++ app/routes/event_questions.js
@@ -0,0 +1,13 @@
+
+const eventQuestions = require("../controllers/event_questions.server.controllers.js")
+
+module.exports = function(app){
+    app.route("")
+        .get();
+
+    app.route("")
+        .get();
+
+    app.route("")
+        .get();
+}
